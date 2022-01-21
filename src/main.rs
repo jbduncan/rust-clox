2e29ff64314a648132d79afc20f39aa32a119bdf
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2022-01-21 18:38:04 +0000

    Do some more cleanups before continuing with Chapter 16

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,43 @@
+use anyhow::Result;
 use rust_clox::vm::{InterpretResult, VM};
-use std::borrow::Borrow;
 use std::env;
 use std::fs::read;
 use std::io::{self, BufRead, Stdout, Write};
 use std::process::exit;
 
-fn main() -> anyhow::Result<()> {
-    exit(run_app()?);
+fn main() {
+    exit(run_app());
 }
 
-fn run_app() -> anyhow::Result<i32> {
+fn run_app() -> i32 {
     let args = env::args().collect::<Vec<String>>();
     match args.len() {
-        1 => repl(),
-        2 => Ok(run_file(&args[1])),
+        1 => {
+            repl().map_or_else(
+                |error| {
+                    eprintln!("{}", error);
+                    70
+                },
+                |_| 0
+            )
+        },
+        2 => {
+            run_file(&args[1])
+        },
         _ => {
             eprintln!("Usage: clox [path]");
-            Ok(64)
+            64
         }
     }
 }
 
-fn repl() -> anyhow::Result<i32> {
+fn repl() -> Result<()> {
     // A real-world REPL should be able to handle multiple lines gracefully.
     let stdin = io::stdin();
     let stdout = io::stdout();
     let mut lines = stdin.lock().lines();
     loop {
-        print_immediately(stdout.borrow(), "> ")?;
+        print_immediately(&stdout, "> ")?;
 
         match lines.next() {
             Some(line) => {
@@ -35,28 +45,27 @@ fn repl() -> anyhow::Result<i32> {
             }
             None => {
                 println!();
-                return Ok(0);
+                return Ok(());
             }
         }
     }
 }
 
-fn print_immediately(mut stdout: &Stdout, text: &str) -> anyhow::Result<()> {
+fn print_immediately(mut stdout: &Stdout, text: &str) -> io::Result<()> {
     print!("{}", text);
-    stdout.flush()?;
-    Ok(())
+    stdout.flush()
 }
 
 fn run_file(path: &str) -> i32 {
-    match read(path) {
-        Ok(source) => match VM::new(source.as_slice()).interpret() {
+    read(path).map_or_else(
+        |error| {
+            eprintln!("{}", error);
+            74
+        },
+        |source| match VM::new(source.as_slice()).interpret() {
             InterpretResult::InterpretOk => 0,
             InterpretResult::InterpretCompileError => 65,
             InterpretResult::InterpretRuntimeError => 70,
         },
-        Err(error) => {
-            eprintln!("{}", error);
-            74
-        }
-    }
+    )
 }
